cc6c6ef8f521853ce344037b7863cadb2b874cbb
olhamish <olgami1402@gmail.com>
2024-05-23 04:20:01 +0300

    feat: add numpy and matrix generator

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,6 @@
+from typing import Dict
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +8,20 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get("/multiply-matrices")
+def multiply_matrices():
+    matrix_size = (10, 10)
+
+    # Генерація випадкових матриць та обчислення їх добутку
+    matrix_a = np.random.rand(*matrix_size)
+    matrix_b = np.random.rand(*matrix_size)
+    product = np.matmul(matrix_a, matrix_b)
+
+    # Повернення результату у вигляді словника
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist(),
+    }
